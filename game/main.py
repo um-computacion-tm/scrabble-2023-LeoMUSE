e4fc68d12f446ff001047ddab01414b636ab9df6
leo <leo@pop-os.localdomain>
2023-09-09 12:12:51 -0300

    Added base main and tests for main

---
+++ game/main.py
@@ -0,0 +1,21 @@
+from game.scrabble import ScrabbleGame
+from game.player import Player
+from game.bagtile import BagTiles
+
+def validate_number_of_players():
+    while True:
+        try:
+            players = int(input("Ingrese el numero de jugadores (2-4): "))
+            if 1 <= players <= 4:
+                return players
+            else:
+                print("El numero de jugadores debe estar entre 2 y 4.")
+        except ValueError:
+            print("Por favor, ingrese un numero valido.")
+
+def main():
+    print("¡Bienvenido a Scrabble!")
+
+    players = validate_number_of_players()
+    scrabble_game = ScrabbleGame(players)
+    scrabble_game.start_game
---
+++ test/test_main.py
@@ -0,0 +1,31 @@
+import unittest
+from unittest.mock import patch
+from io import StringIO
+from game.main import validate_number_of_players
+from game.main import main
+
+class TestMain(unittest.TestCase):
+
+    @patch('builtins.input', side_effect = ["2"])
+
+    def test_number_of_player_valid(self, mock_input):
+        num_players = validate_number_of_players()
+        self.assertEqual(num_players, 2)
+
+    @patch('builtins.input', side_effect = ["0", "5", "3"])
+
+    def test_number_of_player_not_valid_and_valid(self, mock_input):
+        num_players = validate_number_of_players()
+        self.assertEqual(num_players, 3)
+
+    @patch('sys.stdout', new_callable = StringIO)
+    @patch('builtins.input', side_effect = ["0", "5", "3"])
+    def test_main(self, mock_input, mock_stdout):
+        main()
+        self.assertEqual(mock_input.call_count, 3)
+        mock_stdout.seek(0)
+        output = mock_stdout.read()
+        self.assertIn("¡Bienvenido a Scrabble!", output)
+
+if __name__ == '__main__':
+    unittest.main()
