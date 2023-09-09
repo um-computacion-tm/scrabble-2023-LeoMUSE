cab638e9bb880b00bf3a5dd8bbb5411ff57612d8
leo <leo@pop-os.localdomain>
2023-09-09 12:17:04 -0300

    prueba refactoring

--- game/main.py
+++ game/main.py
@@ -2,11 +2,14 @@ from game.scrabble import ScrabbleGame
 from game.player import Player
 from game.bagtile import BagTiles
 
+def is_valid_number_of_players(num):
+    return 1 <= num <= 4
+
 def validate_number_of_players():
     while True:
         try:
             players = int(input("Ingrese el numero de jugadores (2-4): "))
-            if 1 <= players <= 4:
+            if is_valid_number_of_players(players):
                 return players
             else:
                 print("El numero de jugadores debe estar entre 2 y 4.")
