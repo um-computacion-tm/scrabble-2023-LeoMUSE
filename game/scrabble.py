c9dc17b680cc384c0e92121f12d6a48024f53899
leo <leo@pop-os.localdomain>
2023-09-12 12:33:52 -0300

    Added all metod related to time in turns

--- game/scrabble.py
+++ game/scrabble.py
@@ -2,6 +2,8 @@ from game.board import Board
 from game.player import Player
 from game.bagtile import BagTiles
 from game.tile import Tile
+import time
+import threading
 
 class ScrabbleGame:
     def __init__(self, players_count):
@@ -9,6 +11,7 @@ class ScrabbleGame:
         self.bag_tiles = BagTiles()
         self.players = []
         self.current_player = 0
+        self.turn_limit = 60
         for _ in range(players_count):
             self.players.append(Player())
 
@@ -23,5 +26,19 @@ class ScrabbleGame:
         if self.current_player >= len(self.players):
             self.current_player = 0
 
+    def set_time_limit(self, time_limit):
+        self.turn_limit = time_limit
+    
+    def start_timer(self):
+        threading.Thread(target = self._timer_thread).start()
+
+    def _timer_thread(self):
+        current_player = self.players[self.current_player]
+        start_time = time.time()
+        while time.time() - start_time < self.turn_limit:
+            time.sleep(1)
+        print(f"Tiempo agotado para {current_player} - Turno perdido.")
+        self.next_turn()
+        
 if __name__ == '__main__':
     pass
